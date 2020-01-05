78303971f71c5c3c22a1ca4374803a4ec1279b1e
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-05 02:00:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function wakeDog(dogName, dogBreed){
   return `Wake ${dogName} the ${dogBreed}`;
 }
 
- 
+
 
 function leashDog(dogName, dogBreed){
   console.log(`Leash ${dogName} the ${dogBreed}`);
