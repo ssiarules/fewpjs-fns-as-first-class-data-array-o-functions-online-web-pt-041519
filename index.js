044a099e328a94d2784db8e4c39d5962cfe85cd8
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-05 01:46:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function wakeDog(){
+  console.log("Wake Byron the poodle"); 
+}
+
+function leashDog(){
+  console.log("Leash Byron the poodle"); 
+}
+
+function walkToPark(){
+  console.log("Walk to the park with Byron the poodle"); 
+}
+
+function throwFrisbee(){
+  console.log("Throw the frisbee for Byron the poodle"); 
+}
+
+function throwFrisbee(){
+  console.log("Throw the frisbee for Byron the poodle"); 
+}
