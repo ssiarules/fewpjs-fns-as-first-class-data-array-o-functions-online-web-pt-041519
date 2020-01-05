bc97c41f040ca7db4a04fe4a014e4fd6a88eb373
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-05 02:01:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ function leashDog(dogName, dogBreed){
 }
 
 function walkToPark(dogName, dogBreed){
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
 
 function throwFrisbee(dogName, dogBreed){
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return`Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
 
 function walkHome(dogName, dogBreed){
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return `Walk home with ${dogName} the ${dogBreed}`;
 }
 
 function unleashDog(dogName, dogBreed){
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`;
 }
 
 function exerciseDog(dogName, dogBreed){
